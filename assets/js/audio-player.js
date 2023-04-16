fa813bfdba52012e33bae3b10be96c01099788cf
Pieter Smets <mail@pietersmets.be>
2023-04-16 17:48:57 +0200

    audio-player

--- assets/js/audio-player.js
+++ assets/js/audio-player.js
@@ -315,7 +315,7 @@ export function start_audio_player()
         simp_config = ap_simp.dataset.config ? JSON.parse(ap_simp.dataset.config) : {
             shide_top: false, //show/hide album
             shide_btm: false, //show/hide playlist
-            auto_load: false //auto load audio file
+            auto_load: true //auto load audio file
         };
   
         var simp_elem = '';
